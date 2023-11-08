ab107bb019a7dd707b1454a959d662e93fa1646c
Laziz <lazizo2004@gmail.com>
2023-11-08 22:13:41 +0500

    fix Utils.java

--- core/src/main/java/com/epam/Utils.java
+++ core/src/main/java/com/epam/Utils.java
@@ -4,6 +4,6 @@ import java.util.Arrays;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(String... str) {
-       return Arrays.stream(str).allMatch(Utils::isAllPositiveNumbers);
+       return Arrays.stream(str).allMatch(StringUtils::isPositiveNumber);
     }
 }
